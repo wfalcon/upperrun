d70d9fbf1aeb1909ab01c8a8844a20597d249b9f
Влад <wfalcon@mail.ru>
2019-11-10 19:42:23 +0300

    мелкая правка

--- upperrun.py
+++ upperrun.py
@@ -3,7 +3,6 @@ import time
 while 1:
     s = 'это бегущая заглавная буква по строке'
     i = 1
-    f = 'u'
     while i < s.__len__()+1:
         print(s[:i-1]+ (s[i-1:i]).upper() + s[i:]+'\r', end='', flush=True)
         time.sleep(0.2)
