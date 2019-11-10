011dc794540f66ba18fe2d0ff2ead02d452dbd39
Влад <wfalcon@mail.ru>
2019-11-10 19:59:15 +0300

    мелкая оптимизация

--- upperrun.py
+++ upperrun.py
@@ -4,6 +4,6 @@ while 1:
     s = 'это бегущая заглавная буква по строке'
     i = 1
     while i < s.__len__()+1:
-        print(s[:i-1]+ (s[i-1:i]).upper() + s[i:]+'\r', end='', flush=True)
+        print(s[:i-1]+(s[i-1:i]).upper()+s[i:]+'\r', end='', flush=True)
         time.sleep(0.2)
         i += 1
